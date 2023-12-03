e0a29606a7b8e8af27529c93bda586894c58d67a
dsplog <krishna@dsplog.com>
2023-12-03 09:44:05 +0530

    adding support for decimal numbers in malayalam

--- converters/Digit.py
+++ converters/Digit.py
@@ -16,7 +16,7 @@ class Digit:
     007 -> double o 7
     while 003 -> o o 3
     """
-    def __init__(self):
+    def __init__(self,language='en'):
         super().__init__()
         # Regex used to filter out non digits
         self.filter_regex = re.compile("[^0-9]")
@@ -33,13 +33,33 @@ class Digit:
             "8": "eight",
             "9": "nine"
         }
+        # Translation dict to convert digits to text
+        self.trans_dict_malayalam = {
+            "0":  "പൂജ്യം",
+            "1":  "ഒന്ന്",
+            "2":  "രണ്ട്",
+            "3":  "മൂന്ന്",
+            "4":  "നാല്",
+            "5":  "അഞ്ച്",
+            "6":  "ആറ്",
+            "7":  "ഏഴ്",
+            "8":  "എട്ട്",
+            "9":  "ഒമ്പത്"
+        }
+
 
     def convert(self, token: str) -> str:
         # 1 Filter out anything that isn't a digit
         token = self.filter_regex.sub("", token)
         # 2 Check for special case
         if token == "007":
-            return "double o seven"
+            if self.language == 'en' : 
+                return "double o seven"
+            elif self.language == 'ml' : 
+                return "ഡബിൾ ഓ  സെവൻ"
         # 3 & 4 Convert each digit to text and space out the text
-        token = " ".join([self.trans_dict[c] for c in token])
+        if self.language == 'en' :     
+            token = " ".join([self.trans_dict[c] for c in token])
+        elif self.language == 'ml' :   
+            token = " ".join([self.trans_dict_malayalam[c] for c in token])
         return token
